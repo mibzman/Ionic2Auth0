a3012edfff150a6a42ff5e04350e2e9cd4399f8c
mibzmam <27thisotope@gmail.com>
2017-05-19 12:37:10 -0400

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,
+  	public auth: AuthService) {
+
+  }
+
+}
